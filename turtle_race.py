75235b06fad21510c62c9d744c04005be39c70c4
sankaraxi <sankarfrompalani@gmail.com>
2024-04-26 00:21:41 +0530

    turtle_race.py created and modified

---
+++ turtle_race.py
@@ -0,0 +1,32 @@
+import turtle
+import time
+WIDTH, HEIGHT = 500, 500
+
+
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input("Enter the number turtle you want to race (2 -10): ")
+        if racers.isdigit() :
+            racers = int(racers)
+        else:
+            print("Input is not Numeric... Try Again!")
+            continue #takes the execution back to the starting of while loop
+        
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print("Number is not in range between 2 and 10. Try Again!")
+
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title('Turtle Racing!')
+
+racers = get_number_of_racers()
+init_turtle()
+
+racer = turtle.Turtle()
+racer.forward(100)
+time.sleep(5)
